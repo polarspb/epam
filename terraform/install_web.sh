813fbba86632d8a9e8ef9f03eabc5cb751ff20f1
Ilya_Polishchuk <Ilya_Polishchuk@epam.com>
2021-06-10 02:19:25 +0300

    upload new version of main.tf

--- terraform/install_web.sh
+++ terraform/install_web.sh
@@ -6,7 +6,8 @@ apt -y install nfs-common
 apt -y install apache2
 apt -y install wordpress
 apt -y remove wordpress
-wget https://raw.githubusercontent.com/polarspb/epam/main/terraform/conf_sys.sh
+wget https://raw.githubusercontent.com/polarspb/epam/main/terraform/conf_first.sh
+wget https://raw.githubusercontent.com/polarspb/epam/main/terraform/conf_second.sh
 chmod +x conf_sys.sh
 systemctl restart apache2
 systemctl enable apache2
