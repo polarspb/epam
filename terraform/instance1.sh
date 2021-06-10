5c11ea606d4fd10d4033478900e6192430c87232
Ilya_Polishchuk <Ilya_Polishchuk@epam.com>
2021-06-10 11:08:20 +0300

    upload new version of main.tf

--- terraform/instance1.sh
+++ terraform/instance1.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
-sudo rm -r /var/www/html/
-sudo mkdir /var/www/html/
+sudo rm -r /var/www/html/*.*
 echo "Please input EFS name (example: fs-fe0bf385): "
 read STR
 sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport $STR.efs.us-east-2.amazonaws.com:/ /var/www/html
+echo
+echo "Download & Unzip File..."
+echo
 sudo wget https://wordpress.org/latest.tar.gz
 sudo tar -xzf latest.tar.gz
+echo
+echo "Copy Files..."
+echo
 sudo cp -r wordpress/* /var/www/html/
 sudo wget https://raw.githubusercontent.com/polarspb/epam/main/terraform/wp-config.php
+echo
 echo "Please input RDS base name (example: wordpress.cizydixfugvr.us-east-2.rds.amazonaws.com): "
 read STR2
 sudo sed -i 's/localhost/'$STR2'/g' wp-config.php
 sudo mv wp-config.php /var/www/html/wp-config.php
 sudo rm latest.tar.gz
+echo
+echo "Restart Apache..."
+echo
 sudo systemctl restart apache2
 sudo systemctl enable apache2
+echo
--- terraform/instance2.sh
+++ terraform/instance2.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-sudo rm -r /var/www/html/
-sudo mkdir /var/www/html/
-echo "Please input EFS name (example: fs-fe0bf385): "
+sudo rm -r /var/www/html/*.*
+echo -e -n "Please input EFS name (example: fs-fe0bf385): "
 read STR
 sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport $STR.efs.us-east-2.amazonaws.com:/ /var/www/html
+echo
+echo "Restart Apache..."
+echo
 sudo systemctl restart apache2
 sudo systemctl enable apache2
+echo
